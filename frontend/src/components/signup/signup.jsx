03c084862d5fbd4ae20300a870559c7cace33d0e
Mike Criollo <mkcriollo@gmail.com>
2021-09-15 23:59:17 -0400

    Add a slideshow (fix quick fade - need slow fade)

--- frontend/src/components/signup/signup.jsx
+++ frontend/src/components/signup/signup.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
 import "./signup.css";
 
+let slideIndex = 0;
+// showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("mySlides");
+  console.log(slides);
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) slideIndex = 1;
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000);
+}
+
 const Signup = () => {
+  useEffect(() => {
+    showSlides();
+  }, []);
   return (
     <>
       <div className="full-page-su">
@@ -17,20 +36,58 @@ const Signup = () => {
         <div className="app-example"></div>
       </div>
       <div className="info-page">
-        <i class="fas fa-grip-lines-vertical"></i>
-        <div className="info-holder">
-          <div className="left-info">
-            <h2>01.</h2>
-            <h2>INSPIRE OTHERS,</h2>
-            <h2>BE INSPIRED.</h2>
+        <i className="fas fa-grip-lines-vertical"></i>
+        <div className="slideshow-container">
+          <div className="mySlides fade">
+            <div className="info-holder">
+              <div className="left-info">
+                <h2>01.</h2>
+                <h2>INSPIRE OTHERS,</h2>
+                <h2>BE INSPIRED.</h2>
+              </div>
+              <div className="image-slide-holder">
+                <div className="image-slide-01"></div>
+              </div>
+              <div className="right-info">
+                <p>POST YOUR GOALS, ROUTINES, LIFESTYLE</p>
+                <p>SHARE AND LET OTHERS BE INSPIRED</p>
+                <p>BE INSPIRED BY OTHERS VROADS</p>
+              </div>
+            </div>
           </div>
-          <div className="image-slide-holder">
-            <div className="image-slide-01"></div>
+          <div className="mySlides fade">
+            <div className="info-holder2">
+              <div className="left-info">
+                <h2>02.</h2>
+                <h2>PLAN SHORT TIME GOALS,</h2>
+                <h2>PLAN FOREVER GOALS.</h2>
+              </div>
+              <div className="image-slide-holder">
+                <div className="image-slide-02"></div>
+              </div>
+              <div className="right-info">
+                <p>THINK OF A GOAL,</p>
+                <p>JOT IT DOWN,</p>
+                <p>LIVE BY IT NOW.</p>
+              </div>
+            </div>
           </div>
-          <div className="right-info">
-            <p>POST YOUR GOALS, ROUTINES, LIFESTYLE</p>
-            <p>SHARE AND LET OTHERS BE INSPIRED</p>
-            <p>BE INSPIRED BY OTHERS VROADS</p>
+          <div className="mySlides fade">
+            <div className="info-holder3">
+              <div className="left-info3">
+                <h2>03.</h2>
+                <h2>NEVER WORK ALONE,</h2>
+                <h2>WE GOT YOUR BACK.</h2>
+              </div>
+              <div className="image-slide-holder">
+                <div className="image-slide-03"></div>
+              </div>
+              <div className="right-info">
+                <p>WE GET IT,</p>
+                <p>ORGANIZING CAN BE HARD,</p>
+                <p>WE GOT YOUR BACK.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
